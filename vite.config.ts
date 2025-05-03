6bfa55ffc0ec6da73aec2fdb00d6fb9107391375
itachi9018 <itachi9018@gmail.com>
2025-05-03 14:17:49 +0530

    fixing github pages config

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: "/flowstate-manufacturing-suite",
   plugins: [
     react(),
     mode === 'development' &&
